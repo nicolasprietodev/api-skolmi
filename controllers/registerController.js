4487fedf44babe5cfe2e8cce1b3a5320d3a66c5e
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2024-12-10 18:58:31 -0500

    correcion

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -6,27 +6,27 @@ export class RegisterController {
 
   createUser = async (req, res) => {
     try {
-      const { nombre, correo, password, roleId, isActive } = req.body
+      const { nombre, correo, password, roleId } = req.body
       console.log('Datos recibidos para crear usuario', {
         nombre,
         correo,
         password,
-        roleId,
-        isActive
+        roleId
+        
       })
       const newUser = await this.registerModel.createUser({
         nombre,
         correo,
         password,
-        roleId,
-        isActive
+        roleId
+        
       })
       res.status(201).json(newUser)
 
 
     } catch (error) {
       console.error('Error al crear el usuario: ', error)
-      res.status(500).json({ error: 'pepe' })
+      res.status(500).json({ error: error.message })
     }
   }
 }
