c99611a600c3782aa116c486c46e44a289a28c83
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2024-12-10 19:19:07 -0500

    poolBased

--- connection/pool.js
+++ connection/pool.js
@@ -1,19 +1,19 @@
 import mysql from 'mysql2/promise'
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'hackaton'
-});
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: '',
+//     database: 'hackaton'
+// });
 
-const pool1 = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    port: process.env.MYSQL_PORT,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST ?? 'localhost',
+    user: process.env.MYSQL_USER ?? 'root',
+    port: process.env.MYSQL_PORT ?? 3306,
+    password: process.env.MYSQL_PASSWORD ?? '',
+    database: process.env.MYSQL_DATABASE ?? 'hackaton'
   })
 
 export default pool;
