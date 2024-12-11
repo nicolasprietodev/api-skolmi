fcd10c500aa15622fa03710943e8f714cc665d7e
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2024-12-11 00:37:28 -0500

    corsArreglao

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express, { json } from 'express'
 import { createRouters } from './routes/index.js';
 import dotenv from 'dotenv/config.js'
+import { corsMiddleware } from './middleWares/corsMiddleware.js'
 
 export const createApp = (models) => {
     const app = express()
     app.disable('x-powered-by')
     app.use(json())
-
+    app.use(corsMiddleware)
+    
     const { registerRouter, loginRouter } = createRouters(models)
     console.log('holi',models)
 
