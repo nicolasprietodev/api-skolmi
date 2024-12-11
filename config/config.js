9487cb336cfe351b58b48d15e3033460f16dce88
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2024-12-11 17:06:42 -0500

    corsshari1

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ export const {
     COOKIE_OPTIONS = {
         maxAge: 60 * 60 * 1000, 
         httpOnly: true, 
-        secure: true,
+        secure: false,
         sameSite: 'lax'
       }
 } = process.env
