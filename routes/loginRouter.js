df8ff45c6c31543cc8926528a757204f7f38cb81
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2024-12-11 11:02:43 -0500

    loginPrueba7

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { LoginController } from "../controllers/loginController.js";
-import { authenticate } from "../middleWares/authMiddleware.js";
 
 export const createLoginRouter = ({ loginModel }) => {
     const router = Router();
